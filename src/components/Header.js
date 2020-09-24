bb165fc6503e699cdfc5d4d5388ee95dbcc027b7
halit <halitfuatbatur@gmail.com>
2020-09-24 16:39:40 +0300

    change class into function component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from "react";
 
-class Header extends Component {
-
-  render() {
-    return (
-      <Fragment>
-        <h1 className="text-center">Welcome to Flatiron School's Pizzeria</h1>
-      </Fragment>
-    );
-  }
-
+function Header() {
+  return (
+    <Fragment>
+      <h1 className="text-center">Welcome to Flatiron School's Pizzeria</h1>
+    </Fragment>
+  );
 }
 
 export default Header;
